1334e320d6f5b799d1c337fb9bbadf4f419bf410
raulavilesrodriguez <finbravilesr@gmail.com>
2023-08-01 09:10:02 -0500

    modified Currency Converter Code

--- JavaFx_Currency_Converter/src/Converter.java
+++ JavaFx_Currency_Converter/src/Converter.java
@@ -1,11 +1,11 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -24,7 +24,7 @@ public class Converter extends Application{
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Scene scene = new Scene(grid, 300, 275);
+        Scene scene = new Scene(grid, 400, 350);
         stage.setScene(scene);
 
         Text scenetitle = new Text("Currency Converter");
@@ -41,14 +41,34 @@ public class Converter extends Application{
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4); // column 1, row 4
 
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 5);
-        
+        final Text result = new Text();
+        grid.add(result, 1, 5);
+
+        btn.setOnAction(event -> {
+            String valueUsdString = inputField.getCharacters().toString();
+            try {
+                double valueUsd = Double.parseDouble(valueUsdString);
+                double conversionResult = convert(valueUsd);
+                result.setText(String.format("%.2f", conversionResult));
+
+            } catch (NumberFormatException e) {
+                Alert a = new Alert(Alert.AlertType.ERROR);
+                a.setTitle("Error");
+                a.setHeaderText("Invalid Dollar Amount");
+                a.setContentText("Please only use digits");
+                a.showAndWait();
+            }
+        });
+
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private double convert(double value){
+        return value * 0.78;
     }
 
 }
